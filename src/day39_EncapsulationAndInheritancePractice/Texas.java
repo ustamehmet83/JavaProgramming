b0552e7f71cc259635b5de644fea2b00525b65f6
Mehmet Usta <ustamehmet83@hotmail.com>
2022-12-16 23:57:38 +0300

    16.12 group study

--- src/day39_EncapsulationAndInheritancePractice/Texas.java
+++ src/day39_EncapsulationAndInheritancePractice/Texas.java
@@ -1,6 +1,6 @@
 package day39_EncapsulationAndInheritancePractice;
 
-public class Texas extends States{
+public class Texas extends States {
     public Texas(String abbreviation, String politicalParty, String governor, String senator, int population, double stateTax) {
         super("Texas", abbreviation, politicalParty, governor, senator, population, stateTax);
     }
