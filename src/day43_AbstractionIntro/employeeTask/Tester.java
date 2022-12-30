11117fa28f15c245e0db9bfaad5c8111cbbc1d5b
Mehmet Usta <ustamehmet83@hotmail.com>
2022-12-30 18:05:01 +0300

    day43_AbstractionIntro

--- src/day43_AbstractionIntro/employeeTask/Tester.java
+++ src/day43_AbstractionIntro/employeeTask/Tester.java
@@ -1,18 +1,27 @@
 package day43_AbstractionIntro.employeeTask;
 
-public  class Tester extends Employee {
-    public Tester(String name, int age, char gender, int id) {
-        super(name, age, gender, id);
+public  final class Tester extends Employee {
+
+    public Tester(String name, int age, char gender, int id, String jobTitle, double salary) {
+        super(name, age, gender, id, jobTitle, salary);
     }
 
     @Override
     public void work() {
-
+        System.out.println(getName()+" is testing the application");
     }
 
 
     @Override
     public void sleep() {
-
+        System.out.println(getName()+" sleeps 8 hours");
     }
+    public void bugReport(){
+        System.out.println(getName()+" is creating bug reports");
+    }
+
+
+
+
+
 }
