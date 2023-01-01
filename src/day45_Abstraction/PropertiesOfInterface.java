e95efba6728bdcc20b79a5a79368b064e9cf9ef6
Mehmet Usta <ustamehmet83@hotmail.com>
2023-01-01 20:51:43 +0300

    day45 Abstraction-Interface

---
+++ src/day45_Abstraction/PropertiesOfInterface.java
@@ -0,0 +1,38 @@
+package day45_Abstraction;
+
+public interface PropertiesOfInterface {
+    int a=100; // static & final by default
+
+    static int b=200; // final by default
+
+    /*
+    public PropertiesOfInterface(int a){
+        this.a=a;
+    }
+
+    static {
+        b=10;
+    }
+
+    public void method1(){
+        System.out.println("Instance method");
+    }
+     */
+
+
+
+    public static void method2(){
+        System.out.println("Static method");
+    }
+
+    public abstract void method();
+
+    public default void drink(){
+        System.out.println("Default method");
+    }
+
+
+
+
+
+}
